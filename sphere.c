d91705d458103fa0dd682ce45e3d722682efe81d
0402lucky <lucky.dokania0402@gmail.com>
2025-03-06 20:18:11 +0530

    Add files via upload

---
+++ sphere.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+float r,volume;
+float pi=3.14;
+printf("enter the radius of circle");
+scanf("%f",&r);
+volume=pi*r*r*r*4/3;
+printf("the volume is %f",volume);
+return 0;
+}
+
